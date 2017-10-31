8d5ec5e73151e62d8b9ce43b61066f7cb1ad8bc9
apcs2 <31333781+Kkhanal0485@users.noreply.github.com>
2017-10-31 09:17:55 -0500

    no message

--- PHSClubsApp/ViewController.swift
+++ PHSClubsApp/ViewController.swift
@@ -1,11 +1,3 @@
-// ONe
-//  ViewController.swift
-//  PHSClubsApp
-// two dfafdsf
-//  Created by apcs2 on 10/24/17.
-//  Copyright © 2017 apcs2. All rights reserved.
-//   RISHI MADE THIS CHANGE TO TEST STUFF
-// CHANGE
 
 import UIKit
 
@@ -18,13 +10,8 @@ class ViewController: UIViewController
         super.viewDidLoad()
         
     }
-//Good job
 
 }
-<<<<<<< HEAD
+
 //https:api.hoogleyboogley
-///// ANOTHER STUFF
-=======
 
-
->>>>>>> af45bcfc51dbdcdf9e1b7b75be3c73f12cc61340
